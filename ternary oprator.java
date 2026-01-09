d2fd9b61f95ecebb7a9aeb26c8bc71be202f6adb
sowmiya1602 <sowmiyauthirasamy@gmail.com>
2026-01-09 09:54:08 +0530

    Add files via upload

---
+++ ternary oprator.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10;
+	    int b=20;
+	    int max=(a>b)?a:b;
+	    int min=(a<b)?a:b;
+		System.out.println("maximum is:"+max);
+		System.out.println("minimum is:"+min);
+	}
+}
---
+++ uniary operator.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int i=10;
+	    {
+	        i++;
+	     System.out.println("increment:"+i);
+	}
+	{
+	   i--;
+	System.out.println("decrement:"+i);
+	}
+	
+	    
+	}
+}
